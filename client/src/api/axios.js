4b02a70f2386af8054d3b86144da0eee7a9ac067
Prince <kumarprince551178@gmai.com>
2024-05-23 13:22:51 +0530

    MAde changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://kgp-play-git-main-princes-projects-1842cd2f.vercel.app";
+  "https://kgp-play-git-main-princes-projects-1842cd2f.vercel.app/";
 export default axios.create({
   baseURL: BASE_URL,
 });
